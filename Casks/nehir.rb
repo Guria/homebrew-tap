fa53488d8fc2e4f28b26772d969f285a900fec39
Aleksei Gurianov <gurianov@gmail.com>
2026-06-02 01:22:50 +0300

    Update Nehir to version 0.2.1 and update checksum

--- Casks/nehir.rb
+++ Casks/nehir.rb
@@ -1,6 +1,6 @@
 cask "nehir" do
-  version "0.2.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.2.1"
+  sha256 "83ab26308559a3d520cc393b7fc87871bc921f37d85010d07dab966811a3f9e4"
 
   url "https://github.com/guria/nehir/releases/download/v#{version}/Nehir-#{version}.zip"
   name "Nehir"
